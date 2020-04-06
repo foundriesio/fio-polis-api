28ce9bf45bb5162e44c79fec242b5949bd70864b
Milo Casagrande <milo@foundries.io>
2020-04-06 13:49:41 +0200

    Expose Auth and Members
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 */
 
+import auth from './auth';
 import createresponse from './response';
 import factories from './factories';
+import members from './members';
 import users from './users';
 
+export const Auth = auth;
 export const createResponse = createresponse;
 export const Factories = factories;
+export const Members = members;
 export const Organizations = factories;
 export const Users = users;
