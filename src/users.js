855642ed3c44d7938979e117258e6553b59038b0
Milo Casagrande <milo@foundries.io>
2020-07-10 17:03:16 +0200

    Extend user object
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/users.js
+++ src/users.js
@@ -16,4 +16,113 @@ export class Users extends Polis {
   }
 }
 
+/**
+ * Retrieve all api tokens of a user.
+ * @param {Object} options
+ * @param {String} options.uid - The user id.
+ * @param {Object} options.query - Query object for the request.
+ * @param {Object} options.user - The user performing the request.
+ */
+Users.prototype.findApiTokens = async function ({ uid, query, user }) {
+  return this.find({path: `${uid}/api_tokens/`, query, user});
+};
+
+/**
+ * Create a new api token for a user.
+ * @param {Object} options
+ * @param {String} options.uid - The user id.
+ * @param {Object} options.data - The payload to send.
+ * @param {Object} options.query - Query object for the request.
+ * @param {Object} options.user - The user performing the request.
+ */
+Users.prototype.createApiToken = async function ({ uid, data, query, user }) {
+  return this.create({ path: `${uid}/api_tokens/`, data, query, user });
+};
+
+/**
+ * Update an existing api token.
+ * @param {Object} options
+ * @param {String} options.uid - The user id.
+ * @param {String} options.tid - The token id.
+ * @param {Object} options.data - The payload to send.
+ * @param {Object} options.query - Query object for the request.
+ * @param {Object} options.user - The user performing the request.
+ */
+Users.prototype.updateApiToken = async function ({ uid, tid, data, query, user }) {
+  return this.update({ path: `${uid}/api_tokens/${tid}`, data, query, user });
+};
+
+/**
+ * Delete an api token.
+ * @param {Object} options
+ * @param {String} options.uid - The user id.
+ * @param {String} options.tid - The token id.
+ * @param {Object} options.query - Query object for the request.
+ * @param {Object} options.user - The user performing the request.
+ */
+Users.prototype.deleteApiToken = async function ({ uid, tid, query, user }) {
+  return this.remove({ path: `${uid}/api_tokens/${tid}`, query, user });
+};
+
+/**
+ * Retrieve all client credentials of a user.
+ * @param {Object} options
+ * @param {String} options.uid - The user id.
+ * @param {Object} options.query - Query object for the request.
+ * @param {Object} options.user - The user performing the request.
+ */
+Users.prototype.findClientCredentials = async function ({ uid, query, user }) {
+  return this.find({ path: `${uid}/client_credentials/`, query, user });
+};
+
+/**
+ * Retrieve a client credentials of a user by its id.
+ * @param {Object} options
+ * @param {String} options.uid - The user id.
+ * @param {String} options.cid - The credentials id.
+ * @param {Object} options.query - Query object for the request.
+ * @param {Object} options.user - The user performing the request.
+ */
+Users.prototype.findClientCredentialsById = async function ({ uid, cid, query, user }) {
+  return this.find({ path: `${uid}/client_credentials/${cid}`, query, user });
+};
+
+/**
+ * Retrieve a client credentials of a user by its id.
+ * @param {Object} options
+ * @param {String} options.uid - The user id.
+ * @param {String} options.cid - The credentials id.
+ * @param {Object} options.data - The payload to send.
+ * @param {Object} options.query - Query object for the request.
+ * @param {Object} options.user - The user performing the request.
+ */
+Users.prototype.createClientCredentials = async function ({ uid, cid, data, query, user }) {
+  return this.create({ path: `${uid}/client_credentials/${cid}`, data, query, user });
+};
+
+/**
+ * Update a client credentials of a user by its id.
+ * @param {Object} options
+ * @param {String} options.uid - The user id.
+ * @param {String} options.cid - The credentials id.
+ * @param {Object} options.data - The payload to send.
+ * @param {Object} options.query - Query object for the request.
+ * @param {Object} options.user - The user performing the request.
+ */
+Users.prototype.updateClientCredentials = async function ({ uid, cid, data, query, user }) {
+  return this.update({ path: `${uid}/client_credentials/${cid}`, data, query, user });
+};
+
+/**
+ * Remove a client credentials of a user by its id.
+ * @param {Object} options
+ * @param {String} options.uid - The user id.
+ * @param {String} options.cid - The credentials id.
+ * @param {Object} options.query - Query object for the request.
+ * @param {Object} options.user - The user performing the request.
+ */
+Users.prototype.deleteClientCredentials = async function ({ uid, cid, query, user }) {
+  return this.remove({ path: `${uid}/client_credentials/${cid}`, query, user });
+};
+
 export default Users;
