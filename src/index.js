72eb27d0f9789640ab5add7c8a52da9b440a7660
Milo Casagrande <milo@foundries.io>
2020-09-18 15:27:24 +0200

    fix: cleanup exports
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/index.js
+++ src/index.js
@@ -6,21 +6,12 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 */
 
-import auth from './auth';
-import billing from './billing';
-import createresponse from './response';
-import factories from './factories';
-import members from './members';
-import subscriptions from './subscriptions';
-import users from './users';
-import invites from './invites';
-
-export const Auth = auth;
-export const Billing = billing;
-export const createResponse = createresponse;
-export const Factories = factories;
-export const Members = members;
-export const Organizations = factories;
-export const Subscriptions = subscriptions;
-export const Users = users;
-export const Invites = invites;
+export * from './auth';
+export * from './billing';
+export * from './response';
+export * from './factories';
+export * from './members';
+export * from './subscriptions';
+export * from './users';
+export * from './invites';
+export * from './device-authorization';
